e65c3177764e431677f948b6cedf3ccd1a20aac1
seasnail <seasnail8169@gmail.com>
2021-03-22 16:56:50 +0000

    Initial commit

---
+++ src/main/java/meteordevelopment/addons/example/modules/ExampleModule2.java
@@ -0,0 +1,12 @@
+package meteordevelopment.addons.example.modules;
+
+import minegame159.meteorclient.modules.Categories;
+import minegame159.meteorclient.modules.Module;
+
+public class ExampleModule2 extends Module {
+
+    public ExampleModule2() {
+        super(Categories.Player, "example-2", "This is an example module that is in an existing category.");
+    }
+
+}
